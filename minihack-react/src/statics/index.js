63123a229638809097a3059c598e63b5fb16ff29
kai <khai98yn@gmail.com>
2018-10-08 21:18:15 +0700

    change url of server

--- minihack-react/src/statics/index.js
+++ minihack-react/src/statics/index.js
@@ -1,1 +1,1 @@
-export const API_ROOT = process.env.ENVIROMENT == 'local' ? "http://localhost:6969" : "https://react-minihack.herokuapp.com";
+export const API_ROOT = process.env.ENVIROMENT == 'local' ? "http://localhost:6969" : "https://minihack-game-server.herokuapp.com";
